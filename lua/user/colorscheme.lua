dd2f8a24d60946fbc675e65c59dc6d0d26b87b7d
Ashton Fei <yunjia.fei@gmail.com>
2022-10-01 12:53:25 +0800

    feat: set transpant to true for night fox

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,5 @@
 vim.o.background = "dark" -- or "light" for light mode
--- gruvbox
+-- Default Settings for Gruvbox
 require("gruvbox").setup({
 	undercurl = true,
 	underline = true,
@@ -17,5 +17,46 @@ require("gruvbox").setup({
 	transparent_mode = true,
 })
 
--- tokyonight, gruvbox, nightfox
-vim.cmd("colorscheme tokyonight")
+-- Default Settings for NightFox
+require("nightfox").setup({
+	options = {
+		-- Compiled file's destination location
+		compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+		compile_file_suffix = "_compiled", -- Compiled file suffix
+		transparent = true, -- Disable setting background
+		terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+		dim_inactive = false, -- Non focused panes set to alternative background
+		styles = { -- Style to be applied to different syntax groups
+			comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+			conditionals = "NONE",
+			constants = "NONE",
+			functions = "NONE",
+			keywords = "NONE",
+			numbers = "NONE",
+			operators = "NONE",
+			strings = "NONE",
+			types = "NONE",
+			variables = "NONE",
+		},
+		inverse = { -- Inverse highlight for different types
+			match_paren = false,
+			visual = false,
+			search = false,
+		},
+		modules = { -- List of various plugins and additional options
+			-- ...
+		},
+	},
+	palettes = {},
+	specs = {},
+	groups = {},
+})
+
+-- gruvbox
+-- tokyonight
+-- tokyonight-storm
+-- tokyonight-night
+-- tokyonight-moon
+-- tokyonight-day
+-- nightfox, dayfox, dawnfox, dustfox, nordfox, terafox, carbonfox
+vim.cmd("colorscheme carbonfox")
