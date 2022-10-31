3c6e2e3334222d5913a3a7ad4d3e2b46f863e68a
personal <yunjia.fei@gmail.com>
2022-10-31 21:00:38 +0800

    fix: remove duplicated plugins

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nvim-config
+
 My Nvim Configuration
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -81,16 +81,6 @@ return packer.startup(function(use)
 	})
 
 	use({
-		"kylechui/nvim-surround",
-		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	})
-
-	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
 	})
